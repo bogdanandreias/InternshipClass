c4d0855823585937b5c013e99a7a9e228026c9a5
AzureAD\Bogdan-IoanAndreias <bogdanandreias1923@gmail.com>
2021-03-25 22:06:52 +0200

    Handler for delete button to remove the member on client and server added

--- wwwroot/js/addNewcomer.js
+++ wwwroot/js/addNewcomer.js
@@ -21,4 +21,6 @@ $(document).ready(function () {
     $("#clear").click(function () {
         $("#newcomer").val("");
     })
+
+    
 });
---
+++ wwwroot/js/removeNewcomer.js
@@ -0,0 +1,21 @@
+﻿
+$(document).ready(function () {
+
+    $(".delete").click(function () {
+
+        var newcomerId = $(this).attr('memberId');
+        console.log($(".member"));
+
+        $.ajax({
+            url: `/Home/RemoveMember/${newcomerId}`,
+            type: 'DELETE',
+            success: function (data) {
+                $(".member").eq(newcomerId).remove();
+            },
+            error: function (data) {
+                alert(`Failed to remove ${newcomerId}`);          
+            },
+        });
+    })    
+    
+});
