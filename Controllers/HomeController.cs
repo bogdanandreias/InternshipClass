436801c60e21a2b484f36085b058b551f52e98e2
AzureAD\Bogdan-IoanAndreias <bogdanandreias1923@gmail.com>
2021-04-14 10:18:39 +0300

    Removed unnecessary API method from HomeController

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -46,33 +46,5 @@ namespace RazorMvc.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
-
-        [HttpDelete]
-        public void RemoveMember(int index)
-        {
-            intershipService.RemoveMember(index);
-        }
-
-        [HttpGet]
-        public Intern AddMember(string memberName)
-        {
-            Intern intern = new Intern();
-            intern.Name = memberName;
-            intern.DateOfJoin = DateTime.Now;
-            var newMember = intershipService.AddMember(intern);
-            hubContext.Clients.All.SendAsync("AddMember", newMember.Name, newMember.Id);
-            return newMember;
-        }
-
-        [HttpPut]
-        public void UpdateMember(int id, string newName)
-        {
-            Intern intern = new Intern();
-            intern.Id = id;
-            intern.Name = newName;
-            intern.DateOfJoin = DateTime.Now;
-            intershipService.UpdateMember(intern);
-        }
-
     }
 }
