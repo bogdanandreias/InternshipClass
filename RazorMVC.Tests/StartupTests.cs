67cfffee30aa3109c4e4340f54a4115c39c32b8b
AzureAD\Bogdan-IoanAndreias <bogdanandreias1923@gmail.com>
2021-04-15 13:50:29 +0300

    Implemented ConvertDatabaseUrlToHerokuString and add test

---
+++ RazorMVC.Tests/StartupTests.cs
@@ -0,0 +1,26 @@
+﻿using RazorMvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace RazorMVC.Tests
+{
+    public class StartupTests
+    {
+        [Fact]
+        public void ShouldConvertUrlToHerokuString()
+        {
+            //Assume
+            string url = "postgres://wxtoozdfgwmqeg:77330bd1daff0c623e9583fe62ca7e572646d87a521adbb8f3c5f3c68a0b0e26@ec2-99-80-200-225.eu-west-1.compute.amazonaws.com:5432/dbid7964iqr5rc";
+
+            //Act
+            var herokuConnectionString = Startup.ConvertDatabaseUrlToHerokuString(url);
+
+            //Assert
+            Assert.Equal("Server=ec2-99-80-200-225.eu-west-1.compute.amazonaws.com;Port=5432;Database=dbid7964iqr5rc;User Id=wxtoozdfgwmqeg;Password=77330bd1daff0c623e9583fe62ca7e572646d87a521adbb8f3c5f3c68a0b0e26;Pooling=true;SSL Mode=Require;Trust Server Certificate=True;", herokuConnectionString);
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -17,22 +17,45 @@ namespace RazorMvc
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        private string connectionString;
+
+        public Startup(IConfiguration configuration, IWebHostEnvironment env)
         {
             Configuration = configuration;
+            connectionString = env.IsDevelopment() ? Configuration.GetConnectionString("DefaultConnection") : GetConnectionString();
         }
 
         public IConfiguration Configuration { get; }
+
+        private string GetConnectionString()
+        {
+            var envDatabaseUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
+            var herokuConnectionString = ConvertDatabaseUrlToHerokuString(envDatabaseUrl);
+            return herokuConnectionString;
+        }
+
+        public static string ConvertDatabaseUrlToHerokuString(string envDatabaseUrl)
+        {
+            Uri url;
+            bool isUrl = Uri.TryCreate(envDatabaseUrl, UriKind.Absolute, out url);
+            if (isUrl)
+            {
+                return $"Server={url.Host};Port={url.Port};Database={url.LocalPath.Substring(1)};User Id={url.UserInfo.Split(':')[0]};Password={url.UserInfo.Split(':')[1]};Pooling=true;SSL Mode=Require;Trust Server Certificate=True;";
+            }
+
+            throw new FormatException($"Database URL: ${envDatabaseUrl} could not be converted!");
+        }
+
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
 			services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseNpgsql(
-                    Configuration.GetConnectionString("DefaultConnection")));
+                    connectionString));
             services.AddDbContext<InternDbContext>(options =>
                 options.UseNpgsql(
-                    Configuration.GetConnectionString("DefaultConnection")));
+                    connectionString));
             services.AddDatabaseDeveloperPageExceptionFilter();
             services.AddControllersWithViews();
             services.AddScoped<IInternshipService, InternshipDbService>();
