47d0a4c115380eef5e3e09145b3ef5b208f517c4
AzureAD\Bogdan-IoanAndreias <bogdanandreias1923@gmail.com>
2021-04-16 10:35:37 +0300

    Test class updated

--- RazorMVC.Tests/StartupTests.cs
+++ RazorMVC.Tests/StartupTests.cs
@@ -22,5 +22,18 @@ namespace RazorMVC.Tests
             //Assert
             Assert.Equal("Server=ec2-99-80-200-225.eu-west-1.compute.amazonaws.com;Port=5432;Database=dbid7964iqr5rc;User Id=wxtoozdfgwmqeg;Password=77330bd1daff0c623e9583fe62ca7e572646d87a521adbb8f3c5f3c68a0b0e26;Pooling=true;SSL Mode=Require;Trust Server Certificate=True;", herokuConnectionString);
         }
+
+        [Fact]
+        public void ShouldThrowExceptionOnCorruptUrl()
+        {
+            //Assume
+            string url = "Server=127.0.0.1;Port=5432;Database=internshipclass;User Id=internshipclassadmin;Password=dhGKIYVccVqs3qV9wWb3;";
+
+            //Act & Assert
+            var exception = Assert.Throws<FormatException>(() => Startup.ConvertDatabaseUrlToHerokuString(url));
+
+            Assert.StartsWith("Database URL could not be converted! Check this", exception.Message);
+
+        }
     }
 }
